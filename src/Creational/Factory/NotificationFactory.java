823f573a1dfae6985c840539d1999c2e1dbf2dab
AkakiBurjanadze <akakiburjanadze17@gmail.com>
2026-03-21 20:23:11 +0400

    Implement NotificationFactory class

---
+++ src/Creational/Factory/NotificationFactory.java
@@ -0,0 +1,16 @@
+package Creational.Factory;
+
+public class NotificationFactory {
+    public Notification createNotification(NotificationType channel) {
+        if (channel == null) {
+            return null;
+        }
+
+        return switch (channel) {
+            case SMS -> new SmsNotification();
+            case EMAIL -> new EmailNotification();
+            case PUSH -> new PushNotification();
+            default -> throw new IllegalArgumentException("Unknown channel: " + channel);
+        };
+    }
+}
