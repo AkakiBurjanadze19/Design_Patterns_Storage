d48c363af7eee39c70378caaa5d16bef3b8db0c4
AkakiBurjanadze <akakiburjanadze17@gmail.com>
2026-03-21 20:22:28 +0400

    Implement SmsNotification class

---
+++ src/Creational/Factory/SmsNotification.java
@@ -0,0 +1,8 @@
+package Creational.Factory;
+
+public class SmsNotification implements Notification {
+    @Override
+    public void notifyUser(String message) {
+        System.out.println("Sending SMS: " + message);
+    }
+}
