60cab0378e351169a1ac6a9b1cc917c4339d5fee
AkakiBurjanadze <akakiburjanadze17@gmail.com>
2026-03-22 02:14:28 +0400

    Implement concrete Espresso class

---
+++ src/Structural/Decorator/CoffeeShopSystem/Espresso.java
@@ -0,0 +1,13 @@
+package Structural.Decorator.CoffeeShopSystem;
+
+public class Espresso implements Beverage {
+    @Override
+    public String getDescription() {
+        return "Espresso";
+    }
+
+    @Override
+    public double getCost() {
+        return 1.99;
+    }
+}
