1009ff722ad39bed06a1a1a064d06fa039cb7704
AkakiBurjanadze <akakiburjanadze17@gmail.com>
2026-03-21 20:22:15 +0400

    Implement EmailNotification class

---
+++ src/Creational/Factory/EmailNotification.java
@@ -0,0 +1,8 @@
+package Creational.Factory;
+
+public class EmailNotification implements Notification {
+    @Override
+    public void notifyUser(String message) {
+        System.out.println("Sending email: " + message);
+    }
+}
