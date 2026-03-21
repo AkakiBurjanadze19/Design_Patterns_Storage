474ed48f8b7f61b908adfbd9c044324a757b7426
AkakiBurjanadze <akakiburjanadze17@gmail.com>
2026-03-21 23:15:18 +0400

    Implement Adapter class

---
+++ src/Structural/Adapter/WeatherMonitoring/Adapter.java
@@ -0,0 +1,30 @@
+package Structural.Adapter.WeatherMonitoring;
+
+public class Adapter implements City {
+    City city;
+
+    public Adapter(City city) {
+        this.city = city;
+    }
+
+    @Override
+    public double getTemperature() {
+        double temperature = city.getTemperature();
+        return (temperature * 1.8) + 32;
+    }
+
+    @Override
+    public String getTemperatureScale() {
+        return "Fahrenheit";
+    }
+
+    @Override
+    public String getName() {
+        return city.getName();
+    }
+
+    @Override
+    public void setHasWeatherWarning(boolean hasWeatherWarning) {
+        city.setHasWeatherWarning(hasWeatherWarning);
+    }
+}
