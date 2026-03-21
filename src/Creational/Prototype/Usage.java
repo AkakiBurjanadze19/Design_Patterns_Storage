41cad18a251adae5e11bd705de010e3e582bddf7
AkakiBurjanadze <akakiburjanadze17@gmail.com>
2026-03-21 19:43:19 +0400

    Test cloning

---
+++ src/Creational/Prototype/Usage.java
@@ -0,0 +1,23 @@
+package Creational.Prototype;
+
+public class Usage {
+    public static void main(String[] args) throws CloneNotSupportedException {
+        Car car = new Car();
+        car.setBrand("BMW");
+        car.setModel("E39");
+        car.setYear(1998);
+
+        Person person = new Person();
+        person.setFirstName("John");
+        person.setLastName("Doe");
+        person.setAge(25);
+
+        car.setOwner(person);
+
+        Car carCopy = car.clone();
+        carCopy.getOwner().setFirstName("Michael");
+
+        System.out.println(car);
+        System.out.println(carCopy);
+    }
+}
