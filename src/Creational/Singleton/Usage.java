d721d38252dd1161c11530406ec11480204115ad
AkakiBurjanadze <akakiburjanadze17@gmail.com>
2026-03-21 18:24:24 +0400

    Test DatabaseConnectionManager

--- src/Creational/Singleton/Usage.java
+++ src/Creational/Singleton/Usage.java
@@ -1,4 +1,18 @@
 package Creational.Singleton;
 
 public class Usage {
+    public static void main(String[] args) {
+        Thread thread1 = new Thread(() -> {
+           DatabaseConnectionManager db = DatabaseConnectionManager.getInstance();
+           db.connect();
+        });
+
+        Thread thread2 = new Thread(() -> {
+            DatabaseConnectionManager db = DatabaseConnectionManager.getInstance();
+            db.connect();
+        });
+
+        thread1.start();
+        thread2.start();
+    }
 }
