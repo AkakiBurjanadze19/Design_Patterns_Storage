49302f0e7d6153c3ff9c7437ab92d9404a211196
AkakiBurjanadze <akakiburjanadze17@gmail.com>
2026-03-21 19:43:09 +0400

    Implement Person class

---
+++ src/Creational/Prototype/Person.java
@@ -0,0 +1,52 @@
+package Creational.Prototype;
+
+public class Person implements Cloneable {
+    private String firstName;
+    private String lastName;
+    private int age;
+
+    public Person() {}
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    @Override
+    public Person clone() throws CloneNotSupportedException {
+        try {
+            Person person = (Person) super.clone();
+            return person;
+        } catch (CloneNotSupportedException e) {
+            throw new CloneNotSupportedException();
+        }
+    }
+
+    @Override
+    public String toString() {
+        return "Person{" +
+                "firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", age=" + age +
+                '}';
+    }
+}
