e162a72bbf2a613806f5f933ce092c4782c4d6f1
AkakiBurjanadze <akakiburjanadze17@gmail.com>
2026-03-28 18:21:15 +0400

    Implement concrete payment strategy class

---
+++ src/Behavioral/Strategy/PaymentSystem/CreditCardStrategy.java
@@ -0,0 +1,16 @@
+package Behavioral.Strategy.PaymentSystem;
+
+public class CreditCardStrategy implements PaymentStrategy {
+    private String name;
+    private String cardNumber;
+
+    public CreditCardStrategy(String name, String cardNumber) {
+        this.name = name;
+        this.cardNumber = cardNumber;
+    }
+
+    @Override
+    public void pay(int amount) {
+        System.out.println(amount + " paid with Credit Card (User: " + name + ")");
+    }
+}
