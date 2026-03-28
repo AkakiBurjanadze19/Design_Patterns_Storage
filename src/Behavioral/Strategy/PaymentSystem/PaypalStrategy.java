eab7a8dcf9d70e3ee039db18c4bab702ea15ab88
AkakiBurjanadze <akakiburjanadze17@gmail.com>
2026-03-28 18:20:47 +0400

    Implement concrete payment strategy class

---
+++ src/Behavioral/Strategy/PaymentSystem/PaypalStrategy.java
@@ -0,0 +1,14 @@
+package Behavioral.Strategy.PaymentSystem;
+
+public class PaypalStrategy implements PaymentStrategy {
+    private String email;
+
+    public PaypalStrategy(String email) {
+        this.email = email;
+    }
+
+    @Override
+    public void pay(int amount) {
+        System.out.println(amount + " paid using PayPal account: " + email);
+    }
+}
